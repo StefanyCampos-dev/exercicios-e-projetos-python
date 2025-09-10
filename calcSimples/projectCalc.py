865d094f0f557a6d84697e1f6629a110a1524239
StefanyCampos-dev <stefanycaroline49@gmail.com>
2025-09-10 13:20:06 -0300

    praticando
    Refatorado loop principal e menu; adicionada opção de sair com mensagem e confirmação para continuar cálculos

--- calcSimples/projectCalc.py
+++ calcSimples/projectCalc.py
@@ -8,20 +8,27 @@ def main():
             n2 = float(input('Informe o segundo valor do cálculo:  '))
         except ValueError:  # tratamento de erro
             print('Erro! Digite apenas números.')
-            return  # encerra o programa se entrada inválida
+            continue # volta para pedir os números novamente
 
         while True:
             op = menu.menu()  # chama a função menu() que está dentro do módulo menu
 
+            resultado = calculos.calculos(n1, n2, op)
+
             if op == 5:  # opção de sair
                 print('Saindo...')
-                break  # sai do loop do menu e permite reinserir novos números
-
-            resultado = calculos.calculos(n1, n2, op)
+                return  # sai do loop do menu e permite reinserir novos números
 
             # mostra o resultado se não for None
             if resultado is not None:
                 print(f'\nO resultado é: {resultado}\n')
 
+            continuar = input('Continuar?  (s/n): ').strip().lower()
+            if continuar != "s":
+                print('Encerrando calculadora...')
+                return
+            else:
+                break
 if __name__ == "__main__":
     main()
+#corrigir loop e operação
